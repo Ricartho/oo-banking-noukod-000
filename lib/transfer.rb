a4355723e9d7973c2231a76924cf5f443a380033
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-26 19:36:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       self.status = "complete"
-      @list_transfer << self
+     
            
   end 
 end
