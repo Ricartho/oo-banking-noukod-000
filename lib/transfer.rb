cf9fb448cfe14e507dcd49c0b339dc25d5e8987a
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-26 19:48:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,12 @@ class Transfer
     else 
             self.status = "rejected"
           "Transaction rejected. Please check your account balance."
-         
     end
-     
+  end 
+  
+  def reverse_transfer
+    if self.status == "complete"
+      
+    end 
   end 
 end
