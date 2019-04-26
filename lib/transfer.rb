4a43d84f3435941d8a4f265e81876e6c71086691
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-26 19:27:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,8 +13,8 @@ class Transfer
   end 
   
   def execute_transaction
-    @sender.balance -= @amount
-    @receiver.balance += @amount
+    # @sender.balance -= @amount
+    # @receiver.balance += @amount
     self.status = "complete"
   end 
 end
